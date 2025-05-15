8193bbc1ab25bc69e58a0e2685b078bf28135ba4
Matheus Luna <matheusluna151634@gmail.com>
2025-05-15 00:09:13 -0300

    atualizando

--- EstruturaDoBanco.sql
+++ EstruturaDoBanco.sql
@@ -3,62 +3,42 @@ create database db_screen_sound;
 use db_screen_sound;
 
 CREATE TABLE ss_usuario (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE
 );
-
-
 CREATE TABLE ss_playlist (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    usuario_id BIGINT,
-    CONSTRAINT fk_playlist_usuario FOREIGN KEY (usuario_id) REFERENCES ss_usuario(id)
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  usuario_id BIGINT,
+  CONSTRAINT fk_playlist_usuario 
+    FOREIGN KEY (usuario_id) 
+    REFERENCES ss_usuario(id)
 );
-
-
-
-
 CREATE TABLE ss_midia (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    artista VARCHAR(255) NOT NULL,
-    duracao INT NOT NULL,
-    genero VARCHAR(50) NOT NULL
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  titulo VARCHAR(255) NOT NULL,
+  artista VARCHAR(255) NOT NULL,
+  duracao INT NOT NULL,
+  genero VARCHAR(50),
+  tipo_midia VARCHAR(31) NOT NULL
 );
 
 CREATE TABLE ss_playlist_midia (
-    playlist_id BIGINT NOT NULL,
-    midia_id BIGINT NOT NULL,
-    PRIMARY KEY (playlist_id, midia_id),
-    FOREIGN KEY (playlist_id) REFERENCES ss_playlist(id) ON DELETE CASCADE,
-    FOREIGN KEY (midia_id) REFERENCES ss_midia(id) ON DELETE CASCADE
+  playlist_id BIGINT NOT NULL,
+  midia_id BIGINT NOT NULL,
+  PRIMARY KEY (playlist_id, midia_id),
+  CONSTRAINT fk_playlist_midia_playlist 
+    FOREIGN KEY (playlist_id) 
+    REFERENCES ss_playlist(id) 
+    ON DELETE CASCADE,
+  CONSTRAINT fk_playlist_midia_midia 
+    FOREIGN KEY (midia_id) 
+    REFERENCES ss_midia(id) 
+    ON DELETE CASCADE
 );
 
-ALTER TABLE ss_playlist
-ADD COLUMN usuario_id BIGINT;
-
-ALTER TABLE ss_playlist
-ADD CONSTRAINT fk_playlist_usuario FOREIGN KEY (usuario_id) REFERENCES ss_usuario(id);
-
-ALTER TABLE ss_midia
-MODIFY COLUMN genero ENUM('ROCK', 'POP', 'MPB', 'JAZZ', 'CLASSICA');
-
-ALTER TABLE ss_playlist
-ADD CONSTRAINT fk_playlist_usuario FOREIGN KEY (usuario_id) REFERENCES ss_usuario(id);
-
-ALTER TABLE ss_midia
-ADD COLUMN tipo_midia ENUM('MUSICA', 'PODCAST', 'AUDIOBOOK') NOT NULL;
-
-ALTER TABLE ss_midia
-MODIFY COLUMN duracao TIME NOT NULL;
-
-ALTER TABLE ss_midia DROP COLUMN tipo;
 
 
 
-select * from ss_midia where tipo = ?;
 
-
-ALTER TABLE ss_midia
-MODIFY COLUMN duracao BIGINT not null;
